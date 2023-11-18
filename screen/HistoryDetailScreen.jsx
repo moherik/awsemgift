c0045f78ae3a1e4c5558493e6cf2bab4d52ad761
Erik <moherik.maulana@corp.bm.co.id>
2023-11-18 16:46:54 +0700

    fix history detail error

--- screen/HistoryDetailScreen.jsx
+++ screen/HistoryDetailScreen.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Image, Share, StyleSheet, View } from "react-native";
+import { Image, ScrollView, Share, StyleSheet, View } from "react-native";
 import {
   ActivityIndicator,
   Button,
@@ -15,6 +15,10 @@ import * as Clipboard from "expo-clipboard";
 
 import { currency, dateFormat } from "../lib/formatter";
 import api from "../lib/api";
+import useLoader from "../hooks/useLoader";
+import useAuth from "../hooks/useAuth";
+
+import PaymentList from "../components/PaymentList";
 
 import { ORDER_STATUS, giftStatus } from "../constants";
 
@@ -25,8 +29,11 @@ export default function HistoryDetailScreen({ route, navigation }) {
 
   const [orderData, setOrderData] = useState();
   const [loading, setLoading] = useState(true);
+  const [selectedPayment, setSelectedPayment] = useState();
 
   const theme = useTheme();
+  const auth = useAuth();
+  const { showLoader, dismissLoader } = useLoader();
 
   const mapStatus = giftStatus(theme);
 
@@ -56,8 +63,47 @@ export default function HistoryDetailScreen({ route, navigation }) {
   }, []);
 
   async function handlePay() {
-    const url = orderData.paymentData.invoice_url;
-    navigation.navigate("PaymentWebView", { url });
+    if (orderData.paymentData) {
+      const url = orderData.paymentData.invoice_url;
+      navigation.navigate("PaymentWebView", { url });
+    } else {
+      await processNewPayment();
+    }
+  }
+
+  async function processNewPayment() {
+    try {
+      showLoader();
+
+      if (!selectedPayment) {
+        throw new Error("Pilih tipe pembayaran!");
+      }
+
+      await api
+        .post("gifts/order", {
+          productCode: productData.code,
+          phone: item.billInfo1,
+          name: item.billInfo2,
+          note: orderDetail.note,
+          type: selectedPayment,
+        })
+        .then(async (response) => {
+          if (response.status != 200) {
+            throw new Error(response.message || "Gagal menambahkan hadiah");
+          }
+
+          const { url, orderId } = response.data;
+          if (url) {
+            navigation.navigate("PaymentWebView", { url });
+          } else {
+            navigation.navigate("PaymentResult", { orderId });
+          }
+        });
+    } catch (error) {
+      Toast.show(error?.message || "Terjadi kesalahan");
+    } finally {
+      dismissLoader();
+    }
   }
 
   async function handleSharing() {
@@ -94,110 +140,128 @@ export default function HistoryDetailScreen({ route, navigation }) {
   }
 
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <Text variant="bodySmall">Order ID: {item.id}</Text>
-        <IconButton size={14} icon="content-copy" onPress={copyToClipboard} />
-      </View>
-      <Divider />
-      <List.Item
-        title={productData.name}
-        description={productData.code + " - " + productData.group.category.name}
-        left={(props) => (
-          <Image
-            style={{
-              ...props.style,
-              borderRadius: 10,
-              width: 40,
-              height: 40,
-            }}
-            source={{ uri: productData?.group?.logo }}
-          />
-        )}
-        right={(_props) => (
-          <View style={styles.right}>
-            <Chip>
-              <Text style={{ color: mapStatus[item.status].color }}>
-                {mapStatus[item.status]?.label?.toUpperCase()}
-              </Text>
-            </Chip>
-          </View>
-        )}
-      />
-      <List.Item
-        title={mapStatus[item.status]?.info}
-        titleNumberOfLines={3}
-        left={(props) => <List.Icon {...props} icon="help-circle-outline" />}
-      />
-      <List.Item
-        title={
-          <View>
-            <Text>{item.billInfo2}</Text>
-            <Text>{item.billInfo1}</Text>
-          </View>
-        }
-        description="Penerima"
-        descriptionStyle={{ fontSize: 13 }}
-        left={(props) => <List.Icon {...props} icon="gift-open-outline" />}
-      />
-      {orderDetail.note && (
-        <>
-          <View style={styles.divider} />
-          <List.Item
-            title={orderDetail.note}
-            description="Ucapan"
-            descriptionStyle={{ fontSize: 13 }}
-            left={(props) => (
-              <List.Icon {...props} icon="mailbox-open-outline" />
-            )}
-          />
-        </>
-      )}
-      <List.Item
-        title={currency(productData.price + productData.admin)}
-        description="Total Harga"
-        descriptionStyle={{ fontSize: 13 }}
-        left={(props) => <List.Icon {...props} icon="cash-multiple" />}
-      />
-      {orderData?.status == ORDER_STATUS.PENDING ? (
-        <View style={{ width: "100%" }}>
-          <List.Item
-            title={dateFormat(
-              orderData?.paymentData?.expiry_date,
-              "dd MMMM yyyy hh:mm:ss"
-            )}
-            description="Bayar Sebelum"
-            descriptionStyle={{ fontSize: 13 }}
-            left={(props) => <List.Icon {...props} icon="clock-outline" />}
-          />
-          <Button
-            style={{ marginHorizontal: 15, marginVertical: 10 }}
-            mode="contained"
-            icon="cash"
-            onPress={handlePay}
-          >
-            Bayar Sekarang
-          </Button>
+    <ScrollView>
+      <View style={styles.container}>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Text variant="bodySmall">Order ID: {item.id}</Text>
+          <IconButton size={14} icon="content-copy" onPress={copyToClipboard} />
         </View>
-      ) : orderData?.status == ORDER_STATUS.PLACED ? (
-        <View style={{ marginTop: 15 }}>
-          <Button mode="contained" icon="link" onPress={handleSharing}>
-            Bagikan Link
-          </Button>
+        <Divider />
+        <List.Item
+          title={productData.name}
+          description={
+            productData.code + " - " + productData.group.category.name
+          }
+          left={(props) => (
+            <Image
+              style={{
+                ...props.style,
+                borderRadius: 10,
+                width: 40,
+                height: 40,
+              }}
+              source={{ uri: productData?.group?.logo }}
+            />
+          )}
+          right={(_props) => (
+            <View style={styles.right}>
+              <Chip>
+                <Text style={{ color: mapStatus[item.status].color }}>
+                  {mapStatus[item.status]?.label?.toUpperCase()}
+                </Text>
+              </Chip>
+            </View>
+          )}
+        />
+        <List.Item
+          title={mapStatus[item.status]?.info}
+          titleNumberOfLines={3}
+          left={(props) => <List.Icon {...props} icon="help-circle-outline" />}
+        />
+        <List.Item
+          title={
+            <View>
+              <Text>{item.billInfo2}</Text>
+              <Text>{item.billInfo1}</Text>
+            </View>
+          }
+          description="Penerima"
+          descriptionStyle={{ fontSize: 13 }}
+          left={(props) => <List.Icon {...props} icon="gift-open-outline" />}
+        />
+        {orderDetail.note && (
+          <>
+            <View style={styles.divider} />
+            <List.Item
+              title={orderDetail.note}
+              description="Ucapan"
+              descriptionStyle={{ fontSize: 13 }}
+              left={(props) => (
+                <List.Icon {...props} icon="mailbox-open-outline" />
+              )}
+            />
+          </>
+        )}
+        <List.Item
+          title={currency(productData.price + productData.admin)}
+          description="Total Harga"
+          descriptionStyle={{ fontSize: 13 }}
+          left={(props) => <List.Icon {...props} icon="cash-multiple" />}
+        />
+        {orderData?.status == ORDER_STATUS.PENDING ? (
+          <View style={{ width: "100%" }}>
+            {orderData.paymentData ? (
+              <List.Item
+                title={dateFormat(
+                  orderData?.paymentData?.expiry_date,
+                  "dd MMMM yyyy hh:mm:ss"
+                )}
+                description="Bayar Sebelum"
+                descriptionStyle={{ fontSize: 13 }}
+                left={(props) => <List.Icon {...props} icon="clock-outline" />}
+              />
+            ) : (
+              <>
+                <Divider />
+                <Text style={{ paddingHorizontal: 15, marginTop: 10 }}>
+                  Metode Pembayaran
+                </Text>
+                <PaymentList
+                  onSelect={(paymentId) => setSelectedPayment(paymentId)}
+                  price={productData.price + productData.admin}
+                  auth={auth}
+                />
+              </>
+            )}
+            <Button
+              style={{ marginHorizontal: 15, marginVertical: 10 }}
+              mode="contained"
+              icon="cash"
+              onPress={handlePay}
+            >
+              Bayar Sekarang
+            </Button>
+          </View>
+        ) : orderData?.status == ORDER_STATUS.PLACED ? (
+          <View style={{ marginTop: 15 }}>
+            <Button mode="contained" icon="link" onPress={handleSharing}>
+              Bagikan Link
+            </Button>
+          </View>
+        ) : null}
+        <View style={{ ...styles.centered, marginVertical: 15 }}>
+          <Text variant="bodySmall">
+            Order Time: {dateFormat(item.createdAt, "dd MMMM yyyy hh:mm:ss")}
+          </Text>
         </View>
-      ) : null}
-      <View style={{ ...styles.centered, marginTop: 15 }}>
-        <Text variant="bodySmall">
-          Order Time: {dateFormat(item.createdAt, "dd MMMM yyyy hh:mm:ss")}
-        </Text>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
